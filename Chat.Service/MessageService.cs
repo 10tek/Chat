765fd4c3efe65935877cc68f82a5ae93a2719e53
NickSagalbaev <NickSagalbaev@gmail.com>
2019-10-24 19:35:46 +0600

    Добавил сервисы

---
+++ Chat.Service/MessageService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat.Service
+{
+    public class MessageService
+    {
+        private readonly ChatContext context;
+
+        public MessageService(ChatContext context)
+        {
+            this.context = context;
+        }
+
+        public void SendMessage(string message, User user)
+        {
+            var message = new Message
+            {
+                User = user, 
+                Text = message,
+            };
+            context.Message.Add(message);
+            context.SaveChanges();
+        }
+
+        public ICollection<Message> UpdateMessages()
+        {
+            var allMessage = context.Messages.ToList();
+            return allMessage;
+        }
+    }
+}
---
+++ Chat/MessageUI.cs
@@ -0,0 +1,23 @@
+﻿using Chat.Service;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat.UI
+{
+    public class MessageUI
+    {
+        public void Action()
+        {
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    MessageService messageService = new MessageService(context);
+                    messageService.SendMessage();
+                }
+            }
+        }
+    }
+}
