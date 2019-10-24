e479473213a6f079b9adda530f7d50b377a012df
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-10-24 19:34:53 +0600

    Добавил сущности и создал класс контекст

---
+++ Chat.Domain/Message.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat.Domain
+{
+    public class Message : Entity
+    {
+        public string Text { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Chat.Domain/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat.Domain
+{
+    public class User : Entity
+    {
+        public string Login { get; set; }
+    }
+}
