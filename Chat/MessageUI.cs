dbd31ecc367d975644c0159f39b008078164076e
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-10-24 20:16:56 +0600

    Доработал вывод сообщений

--- Chat/MessageUI.cs
+++ Chat/MessageUI.cs
@@ -3,12 +3,14 @@ using Chat.Domain;
 using Chat.Service;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Chat.UI
 {
     public class MessageUI
     {
+        private const int messagePerPage = 20;
         private readonly ChatContext context;
         private readonly MessageService messageService;
 
@@ -19,25 +21,34 @@ namespace Chat.UI
 
         public void Message(User user)
         {
+            ShowMessages();
             while (true)
             {
                 ConsoleKeyInfo key = Console.ReadKey(true);
                 if (key.Key == ConsoleKey.Enter)
                 {
-                    Console.WriteLine("=> ");
+                    Console.WriteLine("\n=> ");
                     var text = Console.ReadLine();
                     messageService.SendMessage(text, user);
                 }
                 else if (key.Key == ConsoleKey.F5)
                 {
-                    Console.Clear();
-                    var messages = messageService.UpdateMessages();
-                    foreach (var message in messages)
-                    {
-                        Console.WriteLine($"[{message.CreationDate.TimeOfDay}]{message.User.Login} : {message.Text}");
-                    }
+                    ShowMessages();
                 }
             }
+        }
+
+        private void ShowMessages()
+        {
+            Console.Clear();
+            var messageCount = messageService.UpdateMessages().Count - messagePerPage;
+
+            var messages = context.Messages.Skip(messageCount).Take(messagePerPage).ToList();
+            foreach(var message in messages) 
+            { 
+                Console.WriteLine($"[{message.CreationDate.TimeOfDay}]{message.User.Login} : {message.Text}");
+            }
+
         }
     }
 }
