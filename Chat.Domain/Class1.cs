6353ee0812f54c895800eca8baea03acc18047aa
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-10-24 19:16:23 +0600

    добавил абстрактный Entity

--- Chat.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Chat.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ Chat.Domain/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat.Domain
+{
+    public abstract class Entity
+    {
+        public Guid Id {get;set;}
+        public DateTime CreationDate { get; set; }
+        public DateTime DeletedDate { get; set; }
+    }
+}
