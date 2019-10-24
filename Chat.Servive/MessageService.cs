e4f26fa410f9d785182952b6a77a170838997df3
NickSagalbaev <NickSagalbaev@gmail.com>
2019-10-24 19:44:23 +0600

    Добавил серис для сообщений

---
+++ Chat.Servive/MessageService.cs
@@ -0,0 +1,36 @@
+﻿using Chat.DataAccess;
+using Chat.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chat.Service
+{
+    public class MessageService
+    {
+        private readonly ChatContext context;
+
+        public MessageService(ChatContext context)
+        {
+            this.context = context;
+        }
+
+        public void SendMessage(string text, User user)
+        {
+            var message = new Message
+            {
+                User = user,
+                Text = text,
+            };
+            context.Messages.Add(message);
+            context.SaveChanges();
+        }
+
+        public ICollection<Message> UpdateMessages()
+        {
+            var allMessage = context.Messages.ToList();
+            return allMessage;
+        }
+    }
+}
